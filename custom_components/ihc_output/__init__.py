e1d93a364bec3d0e2a813f4a107eb3a30d85a78f
K3nnis <86616524+K3nnis@users.noreply.github.com>
2026-01-12 17:49:59 +0100

    Update __init__.py

--- custom_components/ihc_output/__init__.py
+++ custom_components/ihc_output/__init__.py
@@ -6,7 +6,7 @@ from esphome.const import CONF_ID, CONF_PIN
 ihc_ns = cg.esphome_ns.namespace("ihc_output")
 IHCOutputComponent = ihc_ns.class_("IHCOutputComponent", cg.Component)
 
-# Rettet til at acceptere en LISTE af moduler
+
 CONFIG_SCHEMA = cv.ensure_list(
     cv.Schema({
         cv.GenerateID(): cv.declare_id(IHCOutputComponent),
@@ -15,10 +15,11 @@ CONFIG_SCHEMA = cv.ensure_list(
 )
 
 async def to_code(config_list):
-    # Da det nu er en liste, skal vi løbe igennem hvert modul i listen
+    
     for config in config_list:
         var = cg.new_Pvariable(config[CONF_ID])
         await cg.register_component(var, config)
         
         pin = await cg.gpio_pin_expression(config[CONF_PIN])
+
         cg.add(var.set_pin(pin))
